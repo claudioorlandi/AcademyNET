616375013cd3dc912a92112b5c0eeb60c98232a9
claudioorlandi <g.orlandi@betacom.it>
2025-05-15 10:38:08 +0200

    Aggiunta nuovo componente per gestione centralizzata errori

---
+++ src/app/shared/errorservice/errorservice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-errorservice',
+  imports: [],
+  templateUrl: './errorservice.component.html',
+  styleUrl: './errorservice.component.css'
+})
+export class ErrorserviceComponent {
+
+}
